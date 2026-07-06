d9cd9942271fd73ae8957ba355a99de7e3fdef81
Cogito-ergo-sum25 <valencia.rivera.jose.luis@gmail.com>
2026-07-06 14:53:12 -0600

    Firmware example

---
+++ firmware/firmware.ino
@@ -0,0 +1,84 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <RCSwitch.h>
+#include "secrets.h"
+
+// En lugar de escribir el texto plano, le asignas las macros definidas en secrets.h
+const char* ssid = SECRET_SSID;
+const char* password = SECRET_PASS;
+
+// Tu subdominio o IP de internet para el Broker MQTT
+const char* mqtt_server = "192.168.1.210";
+const int mqtt_port = 1883; 
+
+// ... (Todo el resto de tu código de inicialización, setup y loops se queda exactamente igual)
+WiFiClient espClient;
+PubSubClient client(espClient);
+RCSwitch mySwitch = RCSwitch();
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000); 
+  
+  setup_wifi();
+  
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback); 
+  
+  // Transmisor FS1000A al GPIO 13
+  mySwitch.enableTransmit(13);
+  mySwitch.setProtocol(6);
+  mySwitch.setPulseLength(477);
+  mySwitch.setRepeatTransmit(15);
+  
+  Serial.println("\n[-] Esperando mensajes desde broker.cereva.lat...");
+}
+
+void setup_wifi() {
+  delay(10);
+  Serial.print("Conectando Wi-Fi...");
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\n[+] Wi-Fi Conectado exitosamente.");
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String messageTemp;
+  for (int i = 0; i < length; i++) {
+    messageTemp += (char)payload[i];
+  }
+
+  if (String(topic) == "casa/porton/abrir" && messageTemp == "1") {
+    Serial.println("[+] Comando de apertura recibido desde internet!");
+    mySwitch.send(108628005, 28); // Tu código clonado de 28 bits
+    Serial.println("[+] Señal RF 433 MHz enviada al motor SEG.");
+  }
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Conectando al Mosquitto Local...");
+    
+    // ID único para el actuador
+    if (client.connect("ESP32C3_Porton_Actuator")) {
+      Serial.println(" ¡Conectado con éxito!");
+      client.subscribe("casa/porton/abrir");
+    } else {
+      Serial.print(" falló, rc=");
+      Serial.print(client.state());
+      Serial.println(" Reintentando en 5 segundos...");
+      delay(5000);
+    }
+  }
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+}
